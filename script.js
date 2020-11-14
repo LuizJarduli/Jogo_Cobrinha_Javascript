deda15c027dd7791be0dbea0580f4f6fee1111a7
LuizJarduli <luizjarduli@gmail.com>
2020-11-13 23:05:18 -0300

    Adicionando a cobrinha

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//criando o background
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
@@ -7,4 +8,21 @@ function criarBG() {
     context.fillRect(0, 0, 16 * box, 16 * box); // 4 parâmetros sendo 1: posicao horizontal, 2: posicao vertical, 3:altura, 4:largura
 }
 
+
+//criando a cobrinha
+
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
+
+function criarCobrinha() {
+    for (let i = 0; i < snake.length; i++) {
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
 criarBG();
+criarCobrinha();
