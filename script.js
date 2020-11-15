f95f904df08259dae0359b6f8165223208e0c4e1
LuizJarduli <luizjarduli@gmail.com>
2020-11-15 14:23:58 -0300

    Reiniciar após game over

--- script.js
+++ script.js
@@ -59,8 +59,17 @@ function iniciarJogo() {
     for (i = 1; i < snake.length; i++) {
         if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
             clearInterval(jogo);
-            alert('Game Over :(');
+            alert('Game Over :(\r\nPressione Ok para tentar denovo');
             num = 0;
+            for (i = snake.length; i > 0; i--) {
+                snake.pop();
+            }
+            snake[0] = {
+                x: 8 * box,
+                y: 8 * box
+            }
+            jogo = setInterval(iniciarJogo, 100);
+
         }
     }
 
